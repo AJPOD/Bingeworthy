beb825d71b3ad940eed8476d969df326f25679f1
Andrew O'Donnell <2319283o@student.gla.ac.uk>
2019-03-05 00:05:43 +0000

    Models visible from admin interface

--- bingeworthy/admin.py
+++ bingeworthy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from bingeworthy.models import * # sorry was just easier importing all
 
 # Register your models here.
+# this stuff lets the admin interface see and interact with models
+admin.site.register(UserAccount)
+admin.site.register(Show)
+admin.site.register(Viewership)
+admin.site.register(Review)
+admin.site.register(VotesOnReview)
